2211e78f1d2fe7c281c3fafe44e7cf607ca592a7
eswarlella <eswar.lella1995@gmail.com>
2026-02-27 11:37:49 +0530

    Initial commit

---
+++ chapter_01_Basics/03_verify_setup.js
@@ -0,0 +1,17 @@
+console.log(process.platform);
+
+// MAC - DARWIN
+// WINDOWS - WIN32
+// LINUX - LINUX
+
+console.log(process.arch);
+
+//arm64-MAC
+//x64-windows
+
+console.log("Node Version:", process.version);
+
+console.log(process.version);
+
+//Node Version: v22.12.0
+//v22.12.0
---
+++ chapter_02_JavaScript_Concepts/05_Core_Comments_JS.js
@@ -0,0 +1,20 @@
+//This is comment. THis code will not be executed
+
+console.log("Hello!");
+
+/**
+ * This is a multi-line comment
+ * Author : Eswar Lella
+ * Date : 27/02/2026
+ * 
+ * 
+ **/
+
+
+/*
+ * This is a multi-line comment
+ * Author : Eswar Lella
+ * Date : 27/02/2026
+ * 
+ * 
+ */
